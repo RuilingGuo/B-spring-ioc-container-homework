3610ee7edc72dc13e85602422d54d70f854616c0
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-17 21:15:16 +0800

    [ruiling] feat: solution2 setter

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,15 @@ public class Foo {
 
     private Bar bar;
 
-    public Foo(@Lazy Bar bar) {
+    @Autowired
+    public void setBar(Bar bar) {
         this.bar = bar;
     }
+
+    //    public Foo(Bar bar) {
+//        this.bar = bar;
+//    }
+
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
